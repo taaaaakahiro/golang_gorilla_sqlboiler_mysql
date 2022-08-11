937594ff7ef7492b51b083ef84d97dc453f91a13
takahiro <takapi854@gmail.com>
2022-08-11 22:45:13 +0900

    setup gorilla

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	fmt.Println("hello world")
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
 
+func main() {
 	r := mux.NewRouter()
-	// r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/", HomeHandler)
 	// r.HandleFunc("/products", ProductsHandler)
 	// r.HandleFunc("/articles", ArticlesHandler)
 	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
